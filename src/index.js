468d359d8467414321938f1f32d0fa31d112c8a9
sammandanu <sammandanu@gmail.com>
2023-05-11 21:34:36 +0700

    fix: vue 3

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-function addStyles (win, styles) {
-  styles.forEach(style => {
-    let link = win.document.createElement('link');
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('type', 'text/css');
-    link.setAttribute('href', style);
-    win.document.getElementsByTagName('head')[0].appendChild(link);
+function addStyles(win, styles) {
+  styles.forEach((style) => {
+    let link = win.document.createElement("link");
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("type", "text/css");
+    link.setAttribute("href", style);
+    win.document.getElementsByTagName("head")[0].appendChild(link);
   });
 }
 
 const VueHtmlToPaper = {
-  install (Vue, options = {}) {
-    Vue.prototype.$htmlToPaper = (el, localOptions, cb = () => true) => {
-      let defaultName = '_blank', 
-          defaultSpecs = ['fullscreen=yes','titlebar=yes', 'scrollbars=yes'],
-          defaultReplace = true,
-          defaultStyles = []
+  install(Vue, options = {}) {
+    let globals = Vue.prototype || Vue.config.globalProperties;
+    globals.$htmlToPaper = (el, localOptions, cb = () => true) => {
+      let defaultName = "_blank",
+        defaultSpecs = ["fullscreen=yes", "titlebar=yes", "scrollbars=yes"],
+        defaultReplace = true,
+        defaultStyles = [];
       let {
         name = defaultName,
         specs = defaultSpecs,
         replace = defaultReplace,
-        styles = defaultStyles
+        styles = defaultStyles,
       } = options;
 
       // If has localOptions
@@ -33,7 +34,7 @@ const VueHtmlToPaper = {
 
       console.warn(styles);
 
-      specs = !!specs.length ? specs.join(',') : '';
+      specs = !!specs.length ? specs.join(",") : "";
 
       const element = document.getElementById(el);
 
@@ -41,8 +42,8 @@ const VueHtmlToPaper = {
         alert(`Element to print #${el} not found!`);
         return;
       }
-      
-      const url = '';
+
+      const url = "";
       const win = window.open(url, name, specs, replace);
 
       win.document.write(`
@@ -57,7 +58,7 @@ const VueHtmlToPaper = {
       `);
 
       addStyles(win, styles);
-      
+
       setTimeout(() => {
         win.document.close();
         win.focus();
@@ -65,10 +66,10 @@ const VueHtmlToPaper = {
         win.close();
         cb();
       }, 5000);
-        
+
       return true;
     };
-  }
-}
+  },
+};
 
 export default VueHtmlToPaper;
